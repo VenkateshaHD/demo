581f1764e7a7354d9ea4462c471118c8f8c0fa2f
VenkateshaHD <venkyvenkateshahd1999@gmail.com>
2024-12-29 23:15:10 +0530

    added getIp.sh file

--- newShin/basic.sh
+++ newShin/basic.sh
@@ -1,8 +1,22 @@
-echo "Hello Shell script"
-echo "second line"
-echo "This line changed from github"
-venkatesha
-pratheek
-ajay
-charan 
+#!/bin/bash
 
+# Prompt the user for input
+echo -n "Enter Seconds: "
+#read -r sec
+
+sec=4
+
+# Validate that the input is a positive integer
+#if ! [[ "$sec" =~ ^[0-9]+$ ]] || [ "$sec" -le 0 ]; then
+#    echo "Please enter a valid positive integer."
+#    exit 1
+#fi
+
+# Countdown loop
+while [ $sec -gt 0 ]; do
+    clear
+    date +%T
+    echo $sec
+    sleep 1s
+    ((sec--))
+done
---
+++ newShin/getIp.sh
@@ -0,0 +1,1 @@
+ifconfig wlp0s20f3 | grep "inet " | awk '{print $2}'
